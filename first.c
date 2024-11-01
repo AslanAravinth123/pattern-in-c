82cea7bf9dd88d9a3f54a1839eae788512502401
AslanAravinth123 <ramanaravinth123@gmail.com>
2024-11-01 23:24:49 +0530

    added documention

--- first.c
+++ first.c
@@ -4,9 +4,9 @@ int main(){
 
 	int i,j;
 	
-	for(i=1;i<=5;i++)
+	for(i=1;i<=5;i++)// outer loop
 	{
-		for(j=1;j<=i;j++)
+		for(j=1;j<=i;j++)// inner loop
 		{
 			printf("*");
 		}
--- second.c
+++ second.c
@@ -4,9 +4,9 @@ int main(){
 
 	int i,j;
 	
-	for(i=1;i<=5;i++)
+	for(i=1;i<=5;i++)// outer loop
 	{
-		for(j=1;j<=5;j++)
+		for(j=1;j<=5;j++)// inner loop
 		{
 			printf("*");
 		}
